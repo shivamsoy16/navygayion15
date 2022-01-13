ec296833e2edeaa803a3ba621171285f77b3d80f
shivamsoy16 <shivamsoy16@gmail.com>
2022-01-13 12:53:45 +0530

    v0.0.4k(12.1.22)

--- app/src/main/java/com/su/navygayion15/fragment/TableFragment.java
+++ app/src/main/java/com/su/navygayion15/fragment/TableFragment.java
@@ -1,33 +1,21 @@
 package com.su.navygayion15.fragment;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TableLayout;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.su.navygayion15.Elements;
 import com.su.navygayion15.InfoActivity;
 import com.su.navygayion15.R;
+import com.su.navygayion15.customtext;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,58 +23,129 @@ import java.net.URL;
  * create an instance of this fragment.
  */
 public class TableFragment extends Fragment implements View.OnClickListener {
-    String yourTextValue = "text";
-    int numvals = 5;
-    public static final int NAME = 0;
-    public static final int BRANCH = 1;
-    public static final int EMAIL = 2;
-    public static final int REGISTRATION = 3;
-    public static final int ADMISSION = 4;
 
-    String[] details = new String[5];
-    String[] detailVals = new String[5]; // Get the values from JSON here
-/*    public void otab1(View v)
-    {   Button bt1 = v.findViewById(R.id.butt_1);
-        bt1.setOnClickListener(this);
-        Button bt2 = v.findViewById(R.id.butt_2);
-        bt2.setOnClickListener(this);
-        Button bt3 = v.findViewById(R.id.butt_3);
-        bt3.setOnClickListener(this);
-        Button bt4 = v.findViewById(R.id.butt_4);
-        bt4.setOnClickListener(this);
-        Button bt5 = v.findViewById(R.id.butt_5);
-        bt5.setOnClickListener(this);
-        Button bt6 = v.findViewById(R.id.butt_6);
-        bt6.setOnClickListener(this);
-        Button bt7 = v.findViewById(R.id.butt_7);
-        bt7.setOnClickListener(this);
-        Button bt8 = v.findViewById(R.id.butt_8);
-        bt8.setOnClickListener(this);
-        Button bt9 = v.findViewById(R.id.butt_9);
-        bt9.setOnClickListener(this);
-        Button bt10 = v.findViewById(R.id.butt_10);
-        bt10.setOnClickListener(this);
-        Button bt11 = v.findViewById(R.id.butt_11);
-        bt11.setOnClickListener(this);
-        Button bt12 = v.findViewById(R.id.butt_12);
-        bt12.setOnClickListener(this);
-        Button bt13 = v.findViewById(R.id.butt_13);
-        bt13.setOnClickListener(this);
-        Button bt14 = v.findViewById(R.id.butt_14);
-        bt14.setOnClickListener(this);
-        Button bt15 = v.findViewById(R.id.butt_15);
-        bt15.setOnClickListener(this);
-        Button bt16 = v.findViewById(R.id.butt_16);
-        bt16.setOnClickListener(this);
-        Button bt17 = v.findViewById(R.id.butt_17);
-        bt17.setOnClickListener(this);
-        Button bt18 = v.findViewById(R.id.butt_18);
-        bt18.setOnClickListener(this);
-        Button bt19 = v.findViewById(R.id.butt_19);
-        bt19.setOnClickListener(this);
-        Button bt20 = v.findViewById(R.id.butt_20);
-        bt20.setOnClickListener(this);
-    }*/
+
+    public void otab1(View v){
+        ((customtext) v.findViewById(R.id.cut_1)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_2)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_3)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_4)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_5)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_6)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_7)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_8)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_9)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_10)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_11)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_12)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_13)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_14)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_15)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_16)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_17)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_18)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_19)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_20)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_21)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_22)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_23)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_24)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_25)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_26)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_27)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_28)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_29)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_30)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_31)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_32)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_33)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_34)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_35)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_36)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_37)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_38)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_39)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_40)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_41)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_42)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_43)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_44)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_45)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_46)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_47)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_48)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_49)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_50)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_51)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_52)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_53)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_54)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_55)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_56)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_57)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_58)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_59)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_60)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_61)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_62)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_63)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_64)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_65)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_66)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_67)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_68)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_69)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_70)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_71)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_72)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_73)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_74)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_75)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_76)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_77)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_78)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_79)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_80)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_81)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_82)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_83)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_84)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_85)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_86)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_87)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_88)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_89)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_90)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_91)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_92)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_93)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_94)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_95)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_96)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_97)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_98)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_99)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_100)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_101)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_102)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_103)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_104)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_105)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_106)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_107)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_108)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_109)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_110)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_111)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_112)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_113)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_114)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_115)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_116)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_117)).setOnClickListener(this);
+        ((customtext) v.findViewById(R.id.cut_118)).setOnClickListener(this);
+
+    }
 
 
 
@@ -128,8 +187,7 @@ public class TableFragment extends Fragment implements View.OnClickListener {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-//            TextView myTextView = getActivity().findViewById(R.id.textView1);
-//            myTextView.setText(yourTextValue);
+
         }
     }
 
@@ -137,8 +195,8 @@ public class TableFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_table, container, false);
-        /*otab1(view);*/
-       return view;
+        otab1(view);
+        return view;
 
 
     }
@@ -153,7 +211,7 @@ public class TableFragment extends Fragment implements View.OnClickListener {
         Elements.cos = Integer.parseInt(part1[1]);
         int igg1 = (int) Elements.cos;
         String[][] ele = Elements.getSubjects();
-        if (igg1<=16)
+        if (igg1<=118)
             Toast.makeText(getActivity(), "Clicked on Button:- "+goo1+"  or  "+
                     idString+" "+part1[0]+" and "+igg1+" "+ele[igg1-1][2], Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(getActivity(), InfoActivity.class);
--- app/src/main/java/com/su/navygayion15/fragment/TrendFragment.java
+++ app/src/main/java/com/su/navygayion15/fragment/TrendFragment.java
@@ -1,57 +1,23 @@
 package com.su.navygayion15.fragment;
 
-import static android.view.View.inflate;
-
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.Size;
 import androidx.fragment.app.Fragment;
 
-import android.text.Layout;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.su.navygayion15.Elements;
-import com.su.navygayion15.InfoActivity;
 import com.su.navygayion15.R;
-import com.su.navygayion15.customtext;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link TrendFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TrendFragment extends Fragment implements View.OnClickListener {
+public class TrendFragment extends Fragment {
 
-
-    public void otab1(View v)
-    {
-        customtext ct1 = v.findViewById(R.id.cut_1);
-        ct1.setOnClickListener(this);
-        customtext ct11 = v.findViewById(R.id.cut_11);
-        ct11.setOnClickListener(this);
-        customtext ct19 = v.findViewById(R.id.cut_19);
-        ct19.setOnClickListener(this);
-        customtext ct37 = v.findViewById(R.id.cut_37);
-        ct37.setOnClickListener(this);
-        customtext ct2 = v.findViewById(R.id.cut_3);
-        ct2.setOnClickListener(this);
-
-
-    }
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -98,24 +64,10 @@ public class TrendFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_trend, container, false);
-        otab1(view);
         return view;
 
     }
 
 
-    public void onClick(View view) {
-        int goo1 = view.getId();
-        Resources res1 = view.getResources();
-        String idString = res1.getResourceEntryName(goo1);
-        String[] part1 = idString.split("(?<=\\D)(?=\\d)");
-        Elements.cos = Integer.parseInt(part1[1]);
-        int igg1 = (int) Elements.cos;
-        String[][] ele = Elements.getSubjects();
-        if (igg1<=16)
-            Toast.makeText(getActivity(), "Clicked on Button:- "+goo1+"  or  "+
-                    idString+" "+part1[0]+" and "+igg1+" "+ele[igg1-1][2], Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(getActivity(), InfoActivity.class);
-        startActivity(intent);
-    }
+
 }
